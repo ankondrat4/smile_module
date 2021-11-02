a5d0989b9ce8ad668077f74406e8fcc23d560342
ankondrat4 <ankondrat4@gmail.com>
2021-11-02 06:45:18 +0200

    Batch and queue module

--- web/modules/custom/batch_queue/src/Plugin/QueueWorker/NotifiedNewNodeQueue.php
+++ web/modules/custom/batch_queue/src/Plugin/QueueWorker/NotifiedNewNodeQueue.php
@@ -3,7 +3,6 @@
 namespace Drupal\batch_queue\Plugin\QueueWorker;
 
 use Drupal\Core\Queue\QueueWorkerBase;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 
 /**
  * A worker which logs into “DB logs'' information about users that should be
@@ -18,46 +17,16 @@ use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 class NotifiedNewNodeQueue extends QueueWorkerBase {
 
   /**
-   * The logger.
-   *
-   * @var \Psr\Log\LoggerInterface
-   */
-  protected $logger;
-
-  /**
-   * NotifiedNewNodeQueue constructor.
-   *
-   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger
-   *   The logger service the instance should use.
-   */
-  public function __construct(LoggerChannelFactoryInterface $logger) {
-    $this->logger = $logger;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public function processItem($data) {
     if (!empty($data)) {
-      $this->logger->get('batch_queue')->info('User @username should be notified about new node @node_title[@node_id]', [
-        '@username' => $data->author,
-        '@node_title' => $data->title,
-        '@node_id' => $data->nid,
+      \Drupal::logger('batch_queue')->notice('User @username should be notified about new node @node_title[@node_id]', [
+        '@username' => $data['author'],
+        '@node_title' => $data['title'],
+        '@node_id' => $data['nid'],
       ]);
-
-
- /*
-      $node_storage = \Drupal::entityTypeManager()->getStorage('node');
-      if ($node = $node_storage->load($data)) {
-        if ($node->isPublished()) {
-          $node->setUnpublished(TRUE);
-          //$node->setPublished(TRUE);
-          $node->save();
-        }
-      }*/
     }
   }
-
-
 
 }
