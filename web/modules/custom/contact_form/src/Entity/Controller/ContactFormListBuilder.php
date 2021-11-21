6711b23d8e0f0219b3ea61646129ae65a977b51a
ankondrat4 <ankondrat4@gmail.com>
2021-11-21 23:49:34 +0200

    :fatal error contact form module:

---
+++ web/modules/custom/contact_form/src/Entity/Controller/ContactFormListBuilder.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Drupal\contact_form\Entity\Controller;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Routing\UrlGeneratorInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a list controller for contact_form entity.
+ *
+ * @ingroup contact_form
+ */
+class ContactFormListBuilder extends EntityListBuilder {
+
+  /**
+   * The url generator.
+   *
+   * @var \Drupal\Core\Routing\UrlGeneratorInterface
+   */
+  protected $urlGenerator;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity_type.manager')->getStorage($entity_type->id()),
+      $container->get('url_generator')
+    );
+  }
+
+  /**
+   * Constructs a new SmileEntityListBuilder object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The entity storage class.
+   * @param \Drupal\Core\Routing\UrlGeneratorInterface $url_generator
+   *   The url generator.
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, UrlGeneratorInterface $url_generator) {
+    parent::__construct($entity_type, $storage);
+    $this->urlGenerator = $url_generator;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * We override ::render() so that we can add our own content above the table.
+   * parent::render() is where EntityListBuilder creates the table using our
+   * buildHeader() and buildRow() implementations.
+   */
+  public function render() {
+    $build['description'] = [
+      '#markup' => $this->t('Contact form.
+         Manage the fields on the <a href="@adminlink">Admin page</a>.', [
+        '@adminlink' => $this->urlGenerator->generateFromRoute('contact_form.contact_form_settings'),
+      ]),
+    ];
+    $build['table'] = parent::render();
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * Building the header and content lines for the smile entity list.
+   *
+   * Calling the parent::buildHeader() adds a column for the possible actions
+   * and inserts the 'edit' and 'delete' links as defined for the entity type.
+   */
+  public function buildHeader() {
+    $header['id'] = $this->t('ID');
+    $header['email'] = $this->t('Email');
+    $header['phone'] = $this->t('Phone');
+    $header['message'] = $this->t('Message');
+    $header['submission_category'] = $this->t('Submission category');
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    /* @var $entity \Drupal\contact_form\Entity\ContactFormEntity */
+    $row['id'] = $entity->id();
+    $row['email'] = $entity->email->value;
+    $row['phone'] = $entity->phone->value;
+    $row['message'] = $entity->body->value;
+    $row['submission_category'] = $entity->submission_category->value;
+    return $row + parent::buildRow($entity);
+  }
+
+}
